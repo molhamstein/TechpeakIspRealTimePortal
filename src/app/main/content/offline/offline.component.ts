e12f6ee3215f63779b1f3e69810484d29fbf47e1
anas-1995 <anasalazmeh.95@gmail.com>
2018-11-21 16:46:41 +0200

    add mac

--- src/app/main/content/offline/offline.component.ts
+++ src/app/main/content/offline/offline.component.ts
@@ -36,7 +36,12 @@ export class FuseofflineComponent {
     offset: number = 0;
     limit: number = 10;
 
-    colorsLocation=[];
+    colorsLocation = [];
+
+
+
+    startTime;
+    endTime;
 
     private onDestroy$ = new Subject<void>();
 
@@ -50,11 +55,11 @@ export class FuseofflineComponent {
 
     }
 
-    fillColorArrray(){
+    fillColorArrray() {
         for (var index = 0; index < this.locations.length; index++) {
             var element = this.locations[index];
-            this.colorsLocation[element.routerName]=this.mainServ.globalServ.getColor(index);        
-            
+            this.colorsLocation[element.routerName] = this.mainServ.globalServ.getColor(index);
+
         }
     }
 
@@ -91,8 +96,8 @@ export class FuseofflineComponent {
     }
 
 
-    initData() {
-        this.mainServ.APIServ.get("clients/countOfflineUsersIsp?mobile=" + this.form.value['mobile'] + "&location=" + this.form.value['location'].routerName + "&from=" + this.form.value['from'] + "&to=" + this.form.value['to']+ "&ip=" + this.form.value['ip']).subscribe((data: any) => {
+    initData(startTime: Date = null, endTime: Date = null) {
+        this.mainServ.APIServ.get("clients/countOfflineUsersIsp?mobile=" + this.form.value['mobile'] + "&location=" + this.form.value['location'].routerName + "&from=" + this.form.value['from'] + "&to=" + this.form.value['to'] + "&ip=" + this.form.value['ip']).subscribe((data: any) => {
             if (this.mainServ.APIServ.getErrorCode() == 0) {
                 console.log(data['count'])
                 this.count = data['count']
@@ -105,14 +110,55 @@ export class FuseofflineComponent {
     }
 
 
-    search() {
+    search(startTime: Date = null, endTime: Date = null) {
         this.offset = 0;
-        this.initData();
+        this.initData(startTime, endTime);
     }
-    getData(offset, limit, wihtLoder: boolean = true) {
-        console.log("Eeeee");
+    getData(offset, limit, wihtLoder: boolean = true, startTime: Date = null, endTime: Date = null) {
         if (wihtLoder)
             this.loadingIndicator = false;
+        var startHoure = 0;
+        var startMin = 0;
+        var endHoure = 23;
+        var endMin = 59;
+        if (this.form.value['from'] != '') {
+            console.log("startTime")
+            console.log(startTime)
+
+            if (startTime != null && startTime['userTime'] != null) {
+                this.startTime = startTime['userTime'];
+            }
+
+            if (this.startTime != null) {
+                startHoure = this.startTime['hour'];
+                if (this.startTime['meriden'] == 'PM')
+                    startHoure += 12
+
+                startMin = this.startTime['minute'];
+            }
+            this.form.value['from'].setHours(startHoure);
+            this.form.value['from'].setMinutes(startMin);
+
+        }
+
+        if (this.form.value['to'] != '') {
+
+            if (endTime != null && endTime['userTime'] != null) {
+                this.endTime = endTime['userTime'];
+            }
+
+
+            if (this.endTime != null) {
+                endHoure = this.endTime['hour'];
+                if (this.endTime['meriden'] == 'PM')
+                    endHoure += 12
+                endMin = this.endTime['minute'];
+            }
+
+            this.form.value['to'].setHours(endHoure);
+            this.form.value['to'].setMinutes(endHoure);
+        }
+
         this.mainServ.APIServ.get("clients/onlineUsersIsp?mobile=" + this.form.value['mobile'] + "&location=" + this.form.value['location'].routerName + "&from=" + this.form.value['from'] + "&to=" + this.form.value['to'] + "&ip=" + this.form.value['ip'] + "&isExport=3&skip=" + limit * offset).subscribe((data: any) => {
             if (this.mainServ.APIServ.getErrorCode() == 0) {
                 this.loadingIndicator = true;
@@ -123,7 +169,7 @@ export class FuseofflineComponent {
     }
 
     export() {
-        this.mainServ.APIServ.get("clients/onlineUsersIsp?mobile=" + this.form.value['mobile'] + "&location=" + this.form.value['location'].routerName + "&from=" + this.form.value['from'] + "&to=" + this.form.value['to'] + "&ip=" + this.form.value['ip']+ "&isExport=1").subscribe((data: any) => {
+        this.mainServ.APIServ.get("clients/onlineUsersIsp?mobile=" + this.form.value['mobile'] + "&location=" + this.form.value['location'].routerName + "&from=" + this.form.value['from'] + "&to=" + this.form.value['to'] + "&ip=" + this.form.value['ip'] + "&isExport=1").subscribe((data: any) => {
             if (this.mainServ.APIServ.getErrorCode() == 0) {
                 var win = window.open(data.path, '_blank');
                 win.focus();
